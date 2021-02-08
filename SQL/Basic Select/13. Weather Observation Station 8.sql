7bce2bf07656705b4dd78820e22a0f8cd9564586
bgani <ganibdev@gmail.com>
2021-02-08 11:03:22 +0600

    Parenthesis removed, distinct is not a function

--- SQL/Basic Select/13. Weather Observation Station 8.sql
+++ SQL/Basic Select/13. Weather Observation Station 8.sql
@@ -1,5 +1,5 @@
 -- https://www.hackerrank.com/challenges/weather-observation-station-8/problem
 
-SELECT DISTINCT(City) FROM Station 
+SELECT DISTINCT City FROM Station 
 WHERE RIGHT(City, 1) in ('A', 'E', 'I', 'O', 'U') 
 AND LEFT(City, 1) in ('A', 'E', 'I', 'O', 'U');
--- SQL/Basic Select/8. Weather Observation Station 3.sql
+++ SQL/Basic Select/8. Weather Observation Station 3.sql
@@ -1,3 +1,3 @@
 -- https://www.hackerrank.com/challenges/weather-observation-station-3/problem
 
-SELECT DISTINCT(City) FROM Station WHERE Id % 2 = 0
+SELECT DISTINCT City FROM Station WHERE Id % 2 = 0
