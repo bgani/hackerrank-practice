ad141751c74ee05d1fa80c81c6f61259a79bd46b
bgani <ganibdev@gmail.com>
2021-02-08 13:13:03 +0600

    Type of triangle

---
+++ SQL/advanced-select/Type of Triangle.sql
@@ -0,0 +1,10 @@
+-- https://www.hackerrank.com/challenges/what-type-of-triangle/problem
+
+SELECT 
+CASE 
+WHEN (A + B) <= C OR (A + C) <= B OR (B + C) <= A THEN 'Not A Triangle'
+WHEN A = B AND B = C THEN 'Equilateral'
+WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+ELSE 'Scalene'
+END
+FROM Triangles;
